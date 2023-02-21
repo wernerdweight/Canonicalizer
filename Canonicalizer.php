e401645050aad62d5d2ff5a13e81ec9577b03f18
Werner Dweight <wd@wernerdweight.com>
2023-02-21 11:23:43 +0100

    Dependency update

--- Canonicalizer.php
+++ Canonicalizer.php
@@ -5,7 +5,9 @@ namespace WernerDweight\Canonicalizer;
 
 class Canonicalizer
 {
-    /** @var string[][] */
+    /**
+     * @var string[][]
+     */
     private const TRANSLITERATION_MAP = [
         [
             // cyrilic
@@ -30,11 +32,20 @@ class Canonicalizer
             'N', 'n',
         ],
     ];
-    /** @var string */
+
+    /**
+     * @var string
+     */
     private const UNICODE_SPECIAL_CHAR_BLACKLIST = '/[^\x09\x0A\x0D\x20-\x7E\xA0-\x{2FF}\x{370}-\x{10FFFF}]/u';
-    /** @var string[] */
+
+    /**
+     * @var string[]
+     */
     private const SPECIAL_CHAR_BLACKLIST = ['`', '\'', '"', '^', '~'];
-    /** @var string[] */
+
+    /**
+     * @var string[]
+     */
     private const GLIBC_W1250_CHAR_MAP = [
         "\xa5\xa3\xbc\x8c\xa7\x8a\xaa\x8d\x8f\x8e\xaf\xb9\xb3\xbe\x9c\x9a\xba\x9d\x9f\x9e\xbf\xc0\xc1\xc2\xc3\xc4" .
         "\xc5\xc6\xc7\xc8\xc9\xca\xcb\xcc\xcd\xce\xcf\xd0\xd1\xd2\xd3\xd4\xd5\xd6\xd7\xd8\xd9\xda\xdb\xdc\xdd\xde" .
@@ -42,27 +53,32 @@ class Canonicalizer
         "\xfa\xfb\xfc\xfd\xfe\x96",
         'ALLSSSSTZZZallssstzzzRAAAALCCCEEEEIIDDNNOOOOxRUUUUYTsraaaalccceeeeiiddnnooooruuuuyt-',
     ];
-    /** @var string */
-    private const DEFAULT_SEPARATOR = '-';
-    /** @var string */
-    private const DEFAULT_ENDING = '';
-
-    /** @var null|callable */
-    private $afterCallback;
-
-    /** @var null|callable */
-    private $beforeCallback;
-
-    /** @var int */
-    private $maxLength;
 
     /**
-     * Canonicalizer constructor.
-     *
-     * @param int           $maxLength
-     * @param callable|null $beforeCallback
-     * @param callable|null $afterCallback
+     * @var string
      */
+    private const DEFAULT_SEPARATOR = '-';
+
+    /**
+     * @var string
+     */
+    private const DEFAULT_ENDING = '';
+
+    /**
+     * @var null|callable
+     */
+    private $afterCallback;
+
+    /**
+     * @var null|callable
+     */
+    private $beforeCallback;
+
+    /**
+     * @var int
+     */
+    private $maxLength;
+
     public function __construct(int $maxLength, ?callable $beforeCallback = null, ?callable $afterCallback = null)
     {
         $this->maxLength = $maxLength;
@@ -70,33 +86,42 @@ class Canonicalizer
         $this->afterCallback = $afterCallback;
     }
 
-    /**
-     * @param callable|null $beforeCallback
-     *
-     * @return Canonicalizer
-     */
     public function setBeforeCallback(?callable $beforeCallback): self
     {
         $this->beforeCallback = $beforeCallback;
         return $this;
     }
 
-    /**
-     * @param callable|null $afterCallback
-     *
-     * @return Canonicalizer
-     */
     public function setAfterCallback(?callable $afterCallback): self
     {
         $this->afterCallback = $afterCallback;
         return $this;
     }
 
-    /**
-     * @param string $string
-     *
-     * @return string
-     */
+    public function canonicalize(
+        string $string,
+        string $ending = self::DEFAULT_ENDING,
+        string $separator = self::DEFAULT_SEPARATOR
+    ): string {
+        if (null !== $this->beforeCallback) {
+            $beforeCallback = $this->beforeCallback;
+            $string = $beforeCallback($string);
+        }
+        $string = $this->toAscii($string);
+        $string = mb_strtolower($string);
+        /** @var string $string */
+        $string = preg_replace('/[^a-z0-9]+/i', $separator, $string);
+        $string = trim($string, $separator);
+        if (self::DEFAULT_ENDING !== $ending) {
+            $string = $this->createEnding($string, $ending, $separator);
+        }
+        if (null !== $this->afterCallback) {
+            $afterCallback = $this->afterCallback;
+            $string = $afterCallback($string);
+        }
+        return $string;
+    }
+
     private function toAscii(string $string): string
     {
         // transliterate cyrilic and other special chars
@@ -120,13 +145,6 @@ class Canonicalizer
         return $string;
     }
 
-    /**
-     * @param string $string
-     * @param string $ending
-     * @param string $separator
-     *
-     * @return string
-     */
     private function createEnding(string $string, string $ending, string $separator): string
     {
         $ending = $separator . trim($ending, $separator);
@@ -135,36 +153,5 @@ class Canonicalizer
             $string = trim(mb_substr($string, 0, $maxLength), $separator);
         }
         return sprintf('%s%s', $string, $ending);
-    }
-
-    /**
-     * @param string $string
-     * @param string $ending
-     * @param string $separator
-     *
-     * @return string
-     */
-    public function canonicalize(
-        string $string,
-        string $ending = self::DEFAULT_ENDING,
-        string $separator = self::DEFAULT_SEPARATOR
-    ): string {
-        if (null !== $this->beforeCallback) {
-            $beforeCallback = $this->beforeCallback;
-            $string = $beforeCallback($string);
-        }
-        $string = $this->toAscii($string);
-        $string = mb_strtolower($string);
-        /** @var string $string */
-        $string = preg_replace('/[^a-z0-9]+/i', $separator, $string);
-        $string = trim($string, $separator);
-        if (self::DEFAULT_ENDING !== $ending) {
-            $string = $this->createEnding($string, $ending, $separator);
-        }
-        if (null !== $this->afterCallback) {
-            $afterCallback = $this->afterCallback;
-            $string = $afterCallback($string);
-        }
-        return $string;
     }
 }
--- Tests/CanonicalizerTest.php
+++ Tests/CanonicalizerTest.php
@@ -11,26 +11,10 @@ use WernerDweight\Canonicalizer\Canonicalizer;
  */
 class CanonicalizerTest extends TestCase
 {
-    /** @var Canonicalizer */
-    protected $canonicalizer;
-
     /**
-     * @param int           $maxLength
-     * @param callable|null $beforeCallback
-     * @param callable|null $afterCallback
-     *
-     * @return Canonicalizer
+     * @var Canonicalizer
      */
-    protected function getCanonicalizer(
-        int $maxLength,
-        ?callable $beforeCallback = null,
-        ?callable $afterCallback = null
-    ): Canonicalizer {
-        if (null === $this->canonicalizer) {
-            $this->canonicalizer = new Canonicalizer($maxLength, $beforeCallback, $afterCallback);
-        }
-        return $this->canonicalizer;
-    }
+    protected $canonicalizer;
 
     /**
      * Test return type.
@@ -38,7 +22,8 @@ class CanonicalizerTest extends TestCase
     public function testReturnType(): void
     {
         $this->assertIsString(
-            $this->getCanonicalizer(255)->canonicalize('Some fancy text with 2 numbers or whatever...')
+            $this->getCanonicalizer(255)
+                ->canonicalize('Some fancy text with 2 numbers or whatever...')
         );
     }
 
@@ -49,25 +34,29 @@ class CanonicalizerTest extends TestCase
     {
         $this->assertEquals(
             'priserne-zlutoucky-kun-upel-dabelske-ody',
-            $this->getCanonicalizer(255)->canonicalize('Příšerně žluťoučký kůň úpěl ďábelské ódy')
+            $this->getCanonicalizer(255)
+                ->canonicalize('Příšerně žluťoučký kůň úpěl ďábelské ódy')
         );
         $this->assertEquals(
             'a-quick-brown-fox-jumps-over-the-lazy-dog',
-            $this->getCanonicalizer(255)->canonicalize('A quick brown fox jumps over the lazy dog')
+            $this->getCanonicalizer(255)
+                ->canonicalize('A quick brown fox jumps over the lazy dog')
         );
         $this->assertContains(
-            $this->getCanonicalizer(255)->canonicalize(
-                'Falsches Üben von Xylophonmusik quält jeden größeren Zwerg'
-            ),
+            $this->getCanonicalizer(255)
+                ->canonicalize(
+                    'Falsches Üben von Xylophonmusik quält jeden größeren Zwerg'
+                ),
             [
                 'falsches-uben-von-xylophonmusik-qualt-jeden-grosseren-zwerg',
                 'falsches-uben-von-xylophonmusik-qualt-jeden-groseren-zwerg',
             ]
         );
         $this->assertContains(
-            $this->getCanonicalizer(255)->canonicalize(
-                'Le cœur déçu mais l\'âme plutôt naïve, Louÿs rêva de crapaüter en canoë au delà des îles, près du mälström où brûlent les novæ.'
-            ),
+            $this->getCanonicalizer(255)
+                ->canonicalize(
+                    'Le cœur déçu mais l\'âme plutôt naïve, Louÿs rêva de crapaüter en canoë au delà des îles, près du mälström où brûlent les novæ.'
+                ),
             [
                 'le-coeur-decu-mais-l-ame-plutot-naive-louys-reva-de-crapauter-en-canoe-au-dela-des-iles-pres-du-malstrom-ou-brulent-les-novae',
                 'le-coeur-decu-mais-lame-plutot-naive-louys-reva-de-crapauter-en-canoe-au-dela-des-iles-pres-du-malstrom-ou-brulent-les-novae',
@@ -75,29 +64,34 @@ class CanonicalizerTest extends TestCase
         );
         $this->assertEquals(
             'krdel-stastnych-datlov-uci-pri-usti-vahu-mlkveho-kona-obhryzat-koru-a-zrat-cerstve-maso',
-            $this->getCanonicalizer(255)->canonicalize(
-                'Kŕdeľ šťastných ďatľov učí pri ústí Váhu mĺkveho koňa obhrýzať kôru a žrať čerstvé mäso.'
-            )
+            $this->getCanonicalizer(255)
+                ->canonicalize(
+                    'Kŕdeľ šťastných ďatľov učí pri ústí Váhu mĺkveho koňa obhrýzať kôru a žrať čerstvé mäso.'
+                )
         );
         $this->assertEquals(
             'quel-fez-sghembo-copre-davanti',
-            $this->getCanonicalizer(255)->canonicalize('Quel fez sghembo copre davanti')
+            $this->getCanonicalizer(255)
+                ->canonicalize('Quel fez sghembo copre davanti')
         );
         $this->assertEquals(
             'pchnac-w-te-lodz-jeza-lub-osm-skrzyn-fig',
-            $this->getCanonicalizer(255)->canonicalize('Pchnąć w tę łódź jeża lub ośm skrzyń fig')
+            $this->getCanonicalizer(255)
+                ->canonicalize('Pchnąć w tę łódź jeża lub ośm skrzyń fig')
         );
         $this->assertEquals(
             'v-cascach-juga-zil-by-citrus-da-no-falsivyj-ekzempljar',
-            $this->getCanonicalizer(255)->canonicalize(
-                'В чащах юга жил бы цитрус? Да, но фальшивый экземпляр!'
-            )
+            $this->getCanonicalizer(255)
+                ->canonicalize(
+                    'В чащах юга жил бы цитрус? Да, но фальшивый экземпляр!'
+                )
         );
         $this->assertEquals(
             'el-veloz-murcielago-hindu-comia-feliz-cardillo-y-kiwi-la-ciguena-tocaba-el-saxofon-detras-del-palenque-de-paja',
-            $this->getCanonicalizer(255)->canonicalize(
-                'El veloz murciélago hindú comía feliz cardillo y kiwi. La cigüeña tocaba el saxofón detrás del palenque de paja'
-            )
+            $this->getCanonicalizer(255)
+                ->canonicalize(
+                    'El veloz murciélago hindú comía feliz cardillo y kiwi. La cigüeña tocaba el saxofón detrás del palenque de paja'
+                )
         );
     }
 
@@ -108,30 +102,34 @@ class CanonicalizerTest extends TestCase
     {
         $this->assertEquals(
             'priserne-zlutoucky-kun-upel-dabelske-ody-ending',
-            $this->getCanonicalizer(80)->canonicalize(
-                'Příšerně žluťoučký kůň úpěl ďábelské ódy',
-                '-ending'
-            )
+            $this->getCanonicalizer(80)
+                ->canonicalize(
+                    'Příšerně žluťoučký kůň úpěl ďábelské ódy',
+                    '-ending'
+                )
         );
         $this->assertEquals(
             'a-quick-brown-fox-jumps-over-the-lazy-dog-ending',
-            $this->getCanonicalizer(80)->canonicalize('A quick brown fox jumps over the lazy dog', '-ending')
+            $this->getCanonicalizer(80)
+                ->canonicalize('A quick brown fox jumps over the lazy dog', '-ending')
         );
         $this->assertContains(
-            $this->getCanonicalizer(80)->canonicalize(
-                'Falsches Üben von Xylophonmusik quält jeden größeren Zwerg',
-                '-ending'
-            ),
+            $this->getCanonicalizer(80)
+                ->canonicalize(
+                    'Falsches Üben von Xylophonmusik quält jeden größeren Zwerg',
+                    '-ending'
+                ),
             [
                 'falsches-uben-von-xylophonmusik-qualt-jeden-grosseren-zwerg-ending',
                 'falsches-uben-von-xylophonmusik-qualt-jeden-groseren-zwerg-ending',
             ]
         );
         $this->assertContains(
-            $this->getCanonicalizer(80)->canonicalize(
-                'Le cœur déçu mais l\'âme plutôt naïve, Louÿs rêva de crapaüter en canoë au delà des îles, près du mälström où brûlent les novæ.',
-                '-ending'
-            ),
+            $this->getCanonicalizer(80)
+                ->canonicalize(
+                    'Le cœur déçu mais l\'âme plutôt naïve, Louÿs rêva de crapaüter en canoë au delà des îles, près du mälström où brûlent les novæ.',
+                    '-ending'
+                ),
             [
                 'le-coeur-decu-mais-l-ame-plutot-naive-louys-reva-de-crapauter-en-canoe-au-ending',
                 'le-coeur-decu-mais-lame-plutot-naive-louys-reva-de-crapauter-en-canoe-au-ending',
@@ -139,25 +137,29 @@ class CanonicalizerTest extends TestCase
         );
         $this->assertEquals(
             'krdel-stastnych-datlov-uci-pri-usti-vahu-mlkveho-kona-obhryzat-koru-a-zra-ending',
-            $this->getCanonicalizer(80)->canonicalize(
-                'Kŕdeľ šťastných ďatľov učí pri ústí Váhu mĺkveho koňa obhrýzať kôru a žrať čerstvé mäso.',
-                '-ending'
-            )
+            $this->getCanonicalizer(80)
+                ->canonicalize(
+                    'Kŕdeľ šťastných ďatľov učí pri ústí Váhu mĺkveho koňa obhrýzať kôru a žrať čerstvé mäso.',
+                    '-ending'
+                )
         );
         $this->assertEquals(
             'quel-fez-sghembo-copre-davanti-ending',
-            $this->getCanonicalizer(80)->canonicalize('Quel fez sghembo copre davanti', '-ending')
+            $this->getCanonicalizer(80)
+                ->canonicalize('Quel fez sghembo copre davanti', '-ending')
         );
         $this->assertEquals(
             'pchnac-w-te-lodz-jeza-lub-osm-skrzyn-fig-ending',
-            $this->getCanonicalizer(80)->canonicalize('Pchnąć w tę łódź jeża lub ośm skrzyń fig', '-ending')
+            $this->getCanonicalizer(80)
+                ->canonicalize('Pchnąć w tę łódź jeża lub ośm skrzyń fig', '-ending')
         );
         $this->assertEquals(
             'v-cascach-juga-zil-by-citrus-da-no-falsivyj-ekzempljar-ending',
-            $this->getCanonicalizer(80)->canonicalize(
-                'В чащах юга жил бы цитрус? Да, но фальшивый экземпляр!',
-                '-ending'
-            )
+            $this->getCanonicalizer(80)
+                ->canonicalize(
+                    'В чащах юга жил бы цитрус? Да, но фальшивый экземпляр!',
+                    '-ending'
+                )
         );
     }
 
@@ -168,33 +170,37 @@ class CanonicalizerTest extends TestCase
     {
         $this->assertEquals(
             'priserne zlutoucky kun upel dabelske ody',
-            $this->getCanonicalizer(255)->canonicalize(
-                'Příšerně žluťoučký kůň úpěl ďábelské ódy',
-                '',
-                ' '
-            )
+            $this->getCanonicalizer(255)
+                ->canonicalize(
+                    'Příšerně žluťoučký kůň úpěl ďábelské ódy',
+                    '',
+                    ' '
+                )
         );
         $this->assertEquals(
             'a quick brown fox jumps over the lazy dog',
-            $this->getCanonicalizer(255)->canonicalize('A quick brown fox jumps over the lazy dog', '', ' ')
+            $this->getCanonicalizer(255)
+                ->canonicalize('A quick brown fox jumps over the lazy dog', '', ' ')
         );
         $this->assertContains(
-            $this->getCanonicalizer(255)->canonicalize(
-                'Falsches Üben von Xylophonmusik quält jeden größeren Zwerg',
-                '',
-                ' '
-            ),
+            $this->getCanonicalizer(255)
+                ->canonicalize(
+                    'Falsches Üben von Xylophonmusik quält jeden größeren Zwerg',
+                    '',
+                    ' '
+                ),
             [
                 'falsches uben von xylophonmusik qualt jeden grosseren zwerg',
                 'falsches uben von xylophonmusik qualt jeden groseren zwerg',
             ]
         );
         $this->assertContains(
-            $this->getCanonicalizer(255)->canonicalize(
-                'Le cœur déçu mais l\'âme plutôt naïve, Louÿs rêva de crapaüter en canoë au delà des îles, près du mälström où brûlent les novæ.',
-                '',
-                ' '
-            ),
+            $this->getCanonicalizer(255)
+                ->canonicalize(
+                    'Le cœur déçu mais l\'âme plutôt naïve, Louÿs rêva de crapaüter en canoë au delà des îles, près du mälström où brûlent les novæ.',
+                    '',
+                    ' '
+                ),
             [
                 'le coeur decu mais l ame plutot naive louys reva de crapauter en canoe au dela des iles pres du malstrom ou brulent les novae',
                 'le coeur decu mais lame plutot naive louys reva de crapauter en canoe au dela des iles pres du malstrom ou brulent les novae',
@@ -202,27 +208,31 @@ class CanonicalizerTest extends TestCase
         );
         $this->assertEquals(
             'krdel stastnych datlov uci pri usti vahu mlkveho kona obhryzat koru a zrat cerstve maso',
-            $this->getCanonicalizer(255)->canonicalize(
-                'Kŕdeľ šťastných ďatľov učí pri ústí Váhu mĺkveho koňa obhrýzať kôru a žrať čerstvé mäso.',
-                '',
-                ' '
-            )
+            $this->getCanonicalizer(255)
+                ->canonicalize(
+                    'Kŕdeľ šťastných ďatľov učí pri ústí Váhu mĺkveho koňa obhrýzať kôru a žrať čerstvé mäso.',
+                    '',
+                    ' '
+                )
         );
         $this->assertEquals(
             'quel fez sghembo copre davanti',
-            $this->getCanonicalizer(255)->canonicalize('Quel fez sghembo copre davanti', '', ' ')
+            $this->getCanonicalizer(255)
+                ->canonicalize('Quel fez sghembo copre davanti', '', ' ')
         );
         $this->assertEquals(
             'pchnac w te lodz jeza lub osm skrzyn fig',
-            $this->getCanonicalizer(255)->canonicalize('Pchnąć w tę łódź jeża lub ośm skrzyń fig', '', ' ')
+            $this->getCanonicalizer(255)
+                ->canonicalize('Pchnąć w tę łódź jeża lub ośm skrzyń fig', '', ' ')
         );
         $this->assertEquals(
             'v cascach juga zil by citrus da no falsivyj ekzempljar',
-            $this->getCanonicalizer(255)->canonicalize(
-                'В чащах юга жил бы цитрус? Да, но фальшивый экземпляр!',
-                '',
-                ' '
-            )
+            $this->getCanonicalizer(255)
+                ->canonicalize(
+                    'В чащах юга жил бы цитрус? Да, но фальшивый экземпляр!',
+                    '',
+                    ' '
+                )
         );
     }
 
@@ -243,21 +253,24 @@ class CanonicalizerTest extends TestCase
         );
         $this->assertEquals(
             'A-QUICK-BROWN-FOX-JUMPS-OVAR-THA-LAZY-DOG',
-            $this->getCanonicalizer(255)->canonicalize('A quick brown fox jumps over the lazy dog')
+            $this->getCanonicalizer(255)
+                ->canonicalize('A quick brown fox jumps over the lazy dog')
         );
         $this->assertContains(
-            $this->getCanonicalizer(255)->canonicalize(
-                'Falsches Üben von Xylophonmusik quält jeden größeren Zwerg'
-            ),
+            $this->getCanonicalizer(255)
+                ->canonicalize(
+                    'Falsches Üben von Xylophonmusik quält jeden größeren Zwerg'
+                ),
             [
                 'FALSCHAS-UBAN-VON-XYLOPHONMUSIK-QUALT-JADAN-GROSSARAN-ZWARG',
                 'FALSCHAS-UBAN-VON-XYLOPHONMUSIK-QUALT-JADAN-GROSARAN-ZWARG',
             ]
         );
         $this->assertContains(
-            $this->getCanonicalizer(255)->canonicalize(
-                'Le cœur déçu mais l\'âme plutôt naïve, Louÿs rêva de crapaüter en canoë au delà des îles, près du mälström où brûlent les novæ.'
-            ),
+            $this->getCanonicalizer(255)
+                ->canonicalize(
+                    'Le cœur déçu mais l\'âme plutôt naïve, Louÿs rêva de crapaüter en canoë au delà des îles, près du mälström où brûlent les novæ.'
+                ),
             [
                 'LA-COEUR-DECU-MAIS-L-AMA-PLUTOT-NAIVA-LOUYS-REVA-DA-CRAPAUTAR-AN-CANOE-AU-DALA-DAS-ILAS-PRES-DU-MALSTROM-OU-BRULANT-LAS-NOVAE',
                 'LA-COEUR-DECU-MAIS-LAMA-PLUTOT-NAIVA-LOUYS-REVA-DA-CRAPAUTAR-AN-CANOE-AU-DALA-DAS-ILAS-PRES-DU-MALSTROM-OU-BRULANT-LAS-NOVAE',
@@ -265,23 +278,27 @@ class CanonicalizerTest extends TestCase
         );
         $this->assertEquals(
             'KRDAL-STASTNYCH-DATLOV-UCI-PRI-USTI-VAHU-MLKVAHO-KONA-OBHRYZAT-KORU-A-ZRAT-CARSTVE-MASO',
-            $this->getCanonicalizer(255)->canonicalize(
-                'Kŕdeľ šťastných ďatľov učí pri ústí Váhu mĺkveho koňa obhrýzať kôru a žrať čerstvé mäso.'
-            )
+            $this->getCanonicalizer(255)
+                ->canonicalize(
+                    'Kŕdeľ šťastných ďatľov učí pri ústí Váhu mĺkveho koňa obhrýzať kôru a žrať čerstvé mäso.'
+                )
         );
         $this->assertEquals(
             'QUAL-FAZ-SGHAMBO-COPRA-DAVANTI',
-            $this->getCanonicalizer(255)->canonicalize('Quel fez sghembo copre davanti')
+            $this->getCanonicalizer(255)
+                ->canonicalize('Quel fez sghembo copre davanti')
         );
         $this->assertEquals(
             'PCHNAC-W-TE-LODZ-JAZA-LUB-OSM-SKRZYN-FIG',
-            $this->getCanonicalizer(255)->canonicalize('Pchnąć w tę łódź jeża lub ośm skrzyń fig')
+            $this->getCanonicalizer(255)
+                ->canonicalize('Pchnąć w tę łódź jeża lub ośm skrzyń fig')
         );
         $this->assertEquals(
             'V-CASCACH-JUGA-ZIL-BY-CITRUS-DA-NO-FALSIVYJ-EKZEMPLJAR',
-            $this->getCanonicalizer(255)->canonicalize(
-                'В чащах юга жил бы цитрус? Да, но фальшивый экземпляр!'
-            )
+            $this->getCanonicalizer(255)
+                ->canonicalize(
+                    'В чащах юга жил бы цитрус? Да, но фальшивый экземпляр!'
+                )
         );
     }
 
@@ -317,5 +334,16 @@ class CanonicalizerTest extends TestCase
             'priserne-zlutoucky-kun-upel-dabelske-ody',
             $canonicalizer->canonicalize('Příšerně žluťoučký kůň úpěl ďábelské ódy')
         );
+    }
+
+    protected function getCanonicalizer(
+        int $maxLength,
+        ?callable $beforeCallback = null,
+        ?callable $afterCallback = null
+    ): Canonicalizer {
+        if (null === $this->canonicalizer) {
+            $this->canonicalizer = new Canonicalizer($maxLength, $beforeCallback, $afterCallback);
+        }
+        return $this->canonicalizer;
     }
 }
